c8b73c1f012aa303ba27ed52dae6390e69abf616
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-16 01:05:58 +0200

    Create StatePractie component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 // import StateInAction from "./StateInAction";
 // import SimpleEvents from "./SimpleEvents";
-import EventAndState from "./EventAndState";
+//import EventAndState from "./EventAndState";
+import StatePractice from "./StatePractice";
 
 function App() {
   return (
     <div className="App">
-      <EventAndState />
+      <StatePractice />
     </div>
   );
 }
---
+++ src/StatePractice.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class StatePractice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "", imageWidth: "" };
+  }
+
+  handleOnFocus = (event) => {
+    this.setState({
+      message: "By filling the form, you agree to the site term of service!",
+    });
+  };
+
+  handleOnMouse = () => {
+    this.setState({ message: "" });
+  };
+
+  handleSize = (event) => {
+    console.dir(event.target);
+    if (event.target.width > 100) {
+      console.log("Your image is too big!");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <input onFocus={this.handleOnFocus} type="text" />
+        <h3 onMouseEnter={this.handleOnMouse}>{this.state.message}</h3>
+
+        <div>
+          <img
+            onLoad={this.handleSize}
+            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StatePractice;
