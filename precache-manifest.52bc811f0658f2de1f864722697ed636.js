dc98fa86627c3e1e4499a051f246ab2bbc6405b4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-26 12:21:18 +0200

    Updates

---
+++ precache-manifest.52bc811f0658f2de1f864722697ed636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa33432ef3cdfad9f2edf32dcd00d10",
+    "url": "/learning-react-app/index.html"
+  },
+  {
+    "revision": "fbe4cf0af56481679b97",
+    "url": "/learning-react-app/static/css/main.a4387821.chunk.css"
+  },
+  {
+    "revision": "71c1e50f7fe066a4866f",
+    "url": "/learning-react-app/static/js/2.c3baee04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-react-app/static/js/2.c3baee04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe4cf0af56481679b97",
+    "url": "/learning-react-app/static/js/main.c63bac67.chunk.js"
+  },
+  {
+    "revision": "5492f04c4bb08ef8f6f4",
+    "url": "/learning-react-app/static/js/runtime-main.1e998352.js"
+  }
+]);
